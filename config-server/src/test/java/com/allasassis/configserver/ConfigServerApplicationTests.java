1da820f6043b8107f94384a609fd26e400c5a4b6
Allas Assis <allasassis10@gmail.com>
2023-12-28 11:51:20 -0300

    Spring Cloud Config Server created and configurated using a git repository as the config center

---
+++ config-server/src/test/java/com/allasassis/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allasassis.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
