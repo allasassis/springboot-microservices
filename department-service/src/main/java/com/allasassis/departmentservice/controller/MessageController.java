09a80e9cd67d8c0b22cdad6508f93905179da757
Allas Assis <allasassis10@gmail.com>
2023-12-28 12:11:40 -0300

    Request Mapping added in Message Controller

--- department-service/src/main/java/com/allasassis/departmentservice/controller/MessageController.java
+++ department-service/src/main/java/com/allasassis/departmentservice/controller/MessageController.java
@@ -3,10 +3,12 @@ package com.allasassis.departmentservice.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
 @RestController
+@RequestMapping("/api/department")
 public class MessageController {
 
     @Value("${spring.boot.message}")
